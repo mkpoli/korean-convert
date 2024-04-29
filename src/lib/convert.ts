6b8111336d952cfd040957a3a054ae68332b0fe0
mkpoli <git@mkpo.li>
2024-04-29 15:31:43 +0900

    Implement basic conversion

---
+++ src/lib/convert.ts
@@ -0,0 +1,382 @@
+const UNICODE_OFFSET = 44032;
+const UNICODE_MAX = 55215;
+
+export const REVISED_ROMANIZATION_OF_KOREAN = {
+	vowels: [
+		'a', // ㅏ
+		'ae', // ㅐ
+		'ya', // ㅑ
+		'yee', // ㅒ
+		'eo', // ㅓ
+		'e', // ㅔ
+		'yeo', // ㅕ
+		'ye', // ㅖ
+		'o', // ㅗ
+		'wa', // ㅘ
+		'wae', // ㅙ
+		'oe', // ㅚ
+		'yo', // ㅛ
+		'u', // ㅜ
+		'wo', // ㅝ
+		'we', // ㅞ
+		'wi', // ㅟ
+		'yu', // ㅠ
+		'eu', // ㅡ
+		'ui', // ㅢ
+		'i' // ㅣ
+	],
+	consonants: {
+		initial: [
+			'g', // ㄱ
+			'kk', // ㄲ
+			'n', // ㄴ
+			'd', // ㄷ
+			'tt', // ㄸ
+			'r', // ㄹ
+			'm', // ㅁ
+			'b', // ㅂ
+			'pp', // ㅃ
+			's', // ㅅ
+			'ss', // ㅆ
+			'', // ㅇ
+			'j', // ㅈ
+			'jj', // ㅉ
+			'ch', // ㅊ
+			'k', // ㅋ
+			't', // ㅌ
+			'p', // ㅍ
+			'h' // ㅎ
+		],
+		final: [
+			'',
+			'k', // ㄱ
+			'k', // ㄲ
+			'kt', // ㄳ
+			'n', // ㄴ
+			'nt', // ㄵ
+			'nh', // ㄶ
+			't', // ㄷ
+			'l', // ㄹ
+			'lk', // ㄺ
+			'lm', // ㄻ
+			'lp', // ㄼ
+			'lt', // ㄽ
+			'lt', // ㄾ
+			'lp', // ㄿ
+			'lh', // ㅀ
+			'm', // ㅁ
+			'p', // ㅂ
+			'pt', // ㅄ
+			't', // ㅅ
+			'tt', // ㅆ
+			'ng', // ㅇ
+			't', // ㅈ
+			't', // ㅊ
+			'k', // ㅋ
+			't', // ㅌ
+			'p', // ㅍ
+			'h' // ㅎ
+		]
+	}
+};
+
+type Syllable = [
+	/// Initial consonant (初聲 초성)
+	onset: string,
+	/// Medial vowel (中聲 중성)
+	nucleus: string,
+	/// Final consonant (終聲 종성)
+	coda: string
+];
+
+const UNICODE_HANGUL_SYLLABLE_BASE = 0xac00;
+const UNICODE_HANGUL_INITIAL_BASE = 0x1100;
+const UNICODE_HANGUL_MEDIAL_BASE = 0x1161;
+const UNICODE_HANGUL_FINAL_BASE = 0x11a7;
+const HANGUL_INITIAL_COUNT = 19;
+const HANGUL_MEDIAL_COUNT = 21;
+const HANGUL_FINAL_COUNT = 28;
+const HANGUL_NUCLEUS_CODA_COUNT = HANGUL_MEDIAL_COUNT * HANGUL_FINAL_COUNT; // 588
+const HANGUL_SYLLABLE_COUNT = HANGUL_INITIAL_COUNT * HANGUL_NUCLEUS_CODA_COUNT; // 11,172
+
+/**
+ * Separates a single Hangul syllable into its onset (initial), nucleus (medial), and coda (final) parts.
+ * @param hangul A single Hangul character.
+ * @returns An object with properties `onset`, `nucleus`, and `coda` representing the parts of the Hangul syllable.
+ */
+function separateSyllable(hangul: string): Syllable {
+	// return hangul.normalize('NFD').split('') as Syllable;
+	// make sure [a, b, c] and, [a, b] are both 3 elements
+	const normalized = hangul.normalize('NFD').split('');
+	if (normalized.length === 2) {
+		normalized.push('');
+	} else if (normalized.length !== 3) {
+		throw new Error(`Invalid Hangul Syllable "${hangul}"`);
+	}
+	return normalized as Syllable;
+
+	// if (
+	// 	hangul.length !== 1 ||
+	// 	hangul.charCodeAt(0) < UNICODE_HANGUL_SYLLABLE_BASE ||
+	// 	hangul.charCodeAt(0) > UNICODE_HANGUL_SYLLABLE_BASE + HANGUL_SYLLABLE_COUNT - 1
+	// ) {
+	// 	throw new Error(`Invalid Hangul Syllable "${hangul}"`);
+	// }
+
+	// const syllableIndex = hangul.charCodeAt(0) - UNICODE_HANGUL_SYLLABLE_BASE;
+	// const onsetIndex = Math.floor(syllableIndex / HANGUL_NUCLEUS_CODA_COUNT);
+	// const nucleusIndex = Math.floor(
+	// 	(syllableIndex - onsetIndex * HANGUL_NUCLEUS_CODA_COUNT) / HANGUL_FINAL_COUNT
+	// );
+	// const codaIndex = syllableIndex % HANGUL_FINAL_COUNT;
+
+	// // return {
+	// // 	onset: String.fromCharCode(UNICODE_HANGUL_INITIAL_BASE + onsetIndex),
+	// // 	nucleus: String.fromCharCode(UNICODE_HANGUL_MEDIAL_BASE + nucleusIndex),
+	// // 	coda: codaIndex === 0 ? '' : String.fromCharCode(UNICODE_HANGUL_FINAL_BASE + codaIndex)
+	// // };
+}
+
+const onsetMapping: Record<string, string> = {
+	ᄀ: 'k',
+	ᄁ: 'kk',
+	ᄂ: 'n',
+	ᄃ: 't',
+	ᄄ: 'tt',
+	ᄅ: 'r',
+	ᄆ: 'm',
+	ᄇ: 'p',
+	ᄈ: 'pp',
+	ᄉ: 's',
+	ᄊ: 'ss',
+	ᄋ: '',
+	ᄌ: 'ch',
+	ᄍ: 'jj',
+	ᄎ: 'ch',
+	ᄏ: 'kh',
+	ᄐ: 'th',
+	ᄑ: 'ph',
+	ᄒ: 'h'
+};
+
+const nucleusMapping: Record<string, string> = {
+	ᅡ: 'a',
+	ᅢ: 'ae',
+	ᅣ: 'ya',
+	ᅤ: 'yae',
+	ᅥ: 'ŏ',
+	ᅦ: 'e',
+	ᅧ: 'yŏ',
+	ᅨ: 'ye',
+	ᅩ: 'o',
+	ᅪ: 'wa',
+	ᅫ: 'wae',
+	ᅬ: 'oi',
+	ᅭ: 'yo',
+	ᅮ: 'u',
+	ᅯ: 'wŏ',
+	ᅰ: 'we',
+	ᅱ: 'wi',
+	ᅲ: 'yu',
+	ᅳ: 'ŭ',
+	ᅴ: 'ŭi',
+	ᅵ: 'i'
+};
+
+const codaMapping: Record<string, string> = {
+	'': '',
+	ᆨ: 'k',
+	// ᆩ: 'kk',
+	ᆩ: 'k',
+	ᆪ: 'ks',
+	ᆫ: 'n',
+	ᆬ: 'nj',
+	ᆭ: 'nh',
+	ᆮ: 't',
+	ᆯ: 'l',
+	ᆰ: 'k',
+	ᆱ: 'm',
+	ᆲ: 'l',
+	ᆳ: 'l',
+	ᆴ: 'lt',
+	ᆵ: 'lp',
+	ᆶ: 'lh',
+	ᆷ: 'm',
+	ᆸ: 'p',
+	ᆹ: 'ps',
+	// ᆺ: 's',
+	ᆺ: 't',
+	// ᆻ: 'ss',
+	ᆻ: 't',
+	ᆼ: 'ng',
+	ᆽ: 't',
+	ᆾ: 't',
+	ᆿ: 'k',
+	ᇀ: 't',
+	ᇁ: 'p',
+	// ᇂ: 'h'
+	ᇂ: 't'
+};
+
+const HangulSoundRules = new Map<string, string>([
+	['ᆼᄀ', 'ngg'],
+	['ᆼᄃ', 'ngd'],
+	['ᆼᄅ', 'ngn'],
+	['ᆼᄇ', 'ngb'],
+	['ᆼᄌ', 'ngj'],
+	['ᆫᄀ', 'ng'],
+	['ᆫᄃ', 'nd'],
+	['ᆫᄅ', 'nr'],
+	['ᆫᄇ', 'nb'],
+	['ᆫᄌ', 'nj'],
+	['ᆫᄂ', 'll'],
+	['ᄅᄀ', 'lg'],
+	['ᄅᄂ', 'll'],
+	['ᆯᄃ', 'ld'],
+	['ᄅᄅ', 'll'],
+	['ᆯᄇ', 'lb'],
+	['ᄅᄌ', 'lj'],
+	['ᆮᄋ', 'd'],
+	['ᆮᄂ', 'nn'],
+	['ᆮᄅ', 'nn'],
+	['ᆮᄆ', 'nm'],
+	['ᆮᄉ', 'ss'],
+	['ᆾᄆ', 'nm'],
+	['ᆷᄀ', 'mg'],
+	['ᆷᄃ', 'md'],
+	['ᆷᄅ', 'mn'],
+	['ᆷᄇ', 'mb'],
+	['ᆷᄌ', 'mj'],
+	['ᆸᄋ', 'b'],
+	['ᆸᄂ', 'mn'],
+	['ᆸᄅ', 'mr'],
+	['ᆸᄆ', 'mm'],
+	['ᆹᄋ', 'bs'],
+	['ᆺᄋ', 'ss'],
+	['ᆻᄋ', 'ss'],
+	['ᆬᄋ', 'nj'],
+	['ᆭᄋ', 'nh'],
+	['ᆰᄋ', 'lg'],
+	['ᆱᄋ', 'lm'],
+	['ᆲᄋ', 'lb'],
+	['ᆳᄋ', 'ls'],
+	['ᆴᄋ', 'th'],
+	['ᆵᄋ', 'lph'],
+	['ᆶᄋ', 'r'],
+	['ᆨᄋ', 'g'],
+	['ᆨᄂ', 'ngn'],
+	['ᆨᄅ', 'ngn'],
+	['ᆨᄆ', 'ngm']
+]);
+
+/**
+ * Converts decomposed Hangul syllable parts to Romanized string, adjusting for voicing and specific sound rules.
+ * @param hangulParts An array of tuples where each tuple represents the onset, nucleus, and coda of a Hangul syllable.
+ * @returns A string with the Romanized version of the Hangul input.
+ */
+/**
+ * Converts decomposed Hangul syllable parts to Romanized string, adjusting for voicing and specific sound rules.
+ * @param hangulParts An array of tuples where each tuple represents the onset, nucleus, and coda of a Hangul syllable.
+ * @returns A string with the Romanized version of the Hangul input.
+ */
+function romanizeHangul(hangulParts: [string, string, string][]): string {
+	return hangulParts
+		.map(([onset, nucleus, coda], index) => {
+			let romanOnset = onsetMapping[onset];
+			const romanNucleus = nucleusMapping[nucleus];
+			let romanCoda = codaMapping[coda] ?? '';
+
+			// Adjust for voicing of the onset when it follows a voiced sound
+			if (index > 0 && romanNucleus) {
+				const previousCoda = hangulParts[index - 1][2];
+				if (codaMapping[previousCoda] === '') {
+					if (onset === 'ᄀ') romanOnset = 'g';
+					else if (onset === 'ᄃ') romanOnset = 'd';
+					else if (onset === 'ᄇ') romanOnset = 'b';
+				}
+			}
+
+			if (index > 0) {
+				// Remove the initial if sound rule matched (because it already includes the onset)
+				const previousCoda = hangulParts[index - 1][2];
+				const ruleKey = previousCoda + onset;
+				const soundRuleResult = HangulSoundRules.get(ruleKey as any);
+				if (soundRuleResult) {
+					romanOnset = '';
+				}
+			}
+
+			// Apply specific sound rule adjustments between coda of the current syllable and onset of the next syllable
+			if (index < hangulParts.length - 1) {
+				const nextOnset = hangulParts[index + 1][0];
+				const ruleKey = coda + nextOnset;
+				console.log(`Checking rule: ${ruleKey}`);
+				const soundRuleResult = HangulSoundRules.get(ruleKey as any);
+				if (soundRuleResult) {
+					console.log(`Rule matched: ${ruleKey} => ${soundRuleResult}`);
+					romanCoda = soundRuleResult;
+				}
+			}
+
+			return romanOnset + romanNucleus + romanCoda;
+		})
+		.join('');
+}
+
+/**
+ * Converts a Hangul syllable into Revised Romanization of Korean.
+ * @param hangul A single Hangul character.
+ * @returns The Revised Romanization of Korean for the Hangul syllable.
+ */
+function convertSyllable(char: string) {
+	console.log({ char });
+	const syllable = separateSyllable(char);
+
+	return syllable.join(' ');
+
+	// const charCode = char.charCodeAt(0);
+	// const isHangul = charCode >= UNICODE_OFFSET && charCode < UNICODE_MAX;
+	// if (isHangul) {
+	// 	let unicodeOffset = charCode - UNICODE_OFFSET;
+	// 	const trailerOffset = unicodeOffset % REVISED_ROMANIZATION_OF_KOREAN.consonants.final.length;
+	// 	unicodeOffset -= trailerOffset;
+	// 	unicodeOffset /= REVISED_ROMANIZATION_OF_KOREAN.consonants.final.length;
+	// 	const vowelOffset = unicodeOffset % REVISED_ROMANIZATION_OF_KOREAN.vowels.length;
+	// 	unicodeOffset -= vowelOffset;
+	// 	unicodeOffset /= REVISED_ROMANIZATION_OF_KOREAN.vowels.length;
+	// 	const leadOffset = unicodeOffset;
+
+	// 	const result =
+	// 		REVISED_ROMANIZATION_OF_KOREAN.consonants.initial[leadOffset] +
+	// 		REVISED_ROMANIZATION_OF_KOREAN.vowels[vowelOffset] +
+	// 		REVISED_ROMANIZATION_OF_KOREAN.consonants.final[trailerOffset];
+	// 	return result;
+	// }
+	// return char;
+}
+
+/**
+ * Converts a Hangul string into DPRK Romanization.
+ * @param hangul A string containing Hangul characters.
+ */
+export function convertHangulToDPRK(hangul: string): string {
+	const segementer = new Intl.Segmenter('ko', { granularity: 'grapheme' });
+	return hangul
+		.split(/(\p{sc=Hangul}+)/u)
+		.map((word) => {
+			if (word.match(/\p{sc=Hangul}/u)) {
+				const chars = [...segementer.segment(word)];
+				try {
+					return romanizeHangul(chars.map((char) => separateSyllable(char.segment)));
+				} catch (error) {
+					console.error(error);
+					return word;
+				}
+				// return [...segementer.segment(word)].map((char) => convertSyllable(char.segment)).join('');
+			} else {
+				return word;
+			}
+		})
+		.filter(Boolean)
+		.join('');
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,263 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { convertHangulToDPRK } from '$lib/convert';
+	import MdiTransferDown from '~icons/mdi/transfer-down';
+	// import smartquotes from 'smartquotes';
+	import { tick } from 'svelte';
+	// import Suggestions from './Suggestions.svelte';
+
+	// import { dictionary } from '$lib/data';
+
+	let textArea: HTMLTextAreaElement;
+	let input: string = '';
+	let output: string = '';
+
+	// TOOD:
+	input = `붉은기 추켜들고 진격해간다
+	["석남", "Sŏngnam"],
+    ["왕십리", "Wangsimni"],
+    ["굽는다", "kumnŭnda"],
+    ["선로", "sŏllo"],
+    ["인덕원", "Indŏgwŏn"],
+    ["교구동", "Kyogu-dong"],
+    ["고비리", "Kobi-ri"],
+    ["금교", "Kŭmgyo"],
+    ["초도", "Chodo"],
+    ["강동", "Kangdong"],
+    ["칠보산", "Chilbosan"],
+    ["곡산", "Koksan"],
+    ["갑산", "Kapsan"],
+    ["앞산", "Apsan"],
+    ["삿갓봉", "Satkatbong"],
+    ["울산", "Ulsan"],
+    ["은률", "Ŭnryul"],
+    ["닭섬", "Taksŏm"],
+    ["물곬", "Mulkol"],
+    ["붉은바위", "Pulgŭnbawi"],
+    ["앉은바위", "Anjŭnbawi"],
+    ["백마산", "Paengmasan"],
+    ["꽃마을", "Kkonmaŭl"],
+    ["압록강", "Amrokgang"],
+    ["천리마", "Chŏllima"],
+    ["한나산", "Hallasan"],
+    ["전라도", "Jŏlla-do"],
+    ["기대산", "Kittaesan"],
+    ["새별읍", "Saeppyŏl-ŭp"],
+    ["뒤문", "Twinmun"],
+    ["앞언덕", "Ap-ŏndŏk"],
+    ["부엌안골", "Puŏk-angol"],
+    ["판교", "Phan-gyo"],
+    ["방어동", "Pang-ŏ-dong"],
+    ["평안남도 평성시", "Phyŏngannam-do Phyŏngsŏng-si"]
+	`;
+
+	const PUNCTUATIONS: Record<string, string> = {
+		'.': '。',
+		',': '，',
+		':': '：',
+		'“': '「',
+		'”': '」'
+	};
+
+	/* TODO: sitelen toki pona -> sitelen munjan */
+	function convertTokiPona2MunJan(tokiPona: string, dictionary: Record<string, string>): string {
+		const words = tokiPona
+			.split(/([\W])/)
+			.filter((word) => word !== ' ')
+			.map((word) => (word in PUNCTUATIONS ? PUNCTUATIONS[word] : word));
+		return words.map((word) => (dictionary[word] ? dictionary[word] : word)).join('');
+	}
+
+	$: if (input) {
+		output = convertHangulToDPRK(input);
+	}
+
+	// $: if ($dictionary && input) {
+	// 	output = convertTokiPona2MunJan(smartquotes(input), $dictionary);
+	// }
+
+	// let suggestionBox: HTMLDivElement;
+
+	// let suggestions: string[] = ['a', 'b', 'c'];
+
+	// // function getLastChar()
+
+	// function compileSuggestions(
+	// 	input: string,
+	// 	caretPos: number,
+	// 	dictionary: Record<string, string>
+	// ): string[] {
+	// 	// Get caret position
+	// 	console.log('carretPos', caretPos);
+	// 	console.log('inputLength', input.length);
+
+	// 	// Get substring until caret position
+	// 	const sub = input.slice(0, caretPos);
+	// 	console.log('sub', sub);
+
+	// 	// Get the last word
+	// 	const lastWord = sub.split(/[\s\W]/).pop();
+
+	// 	if (!lastWord) return [];
+	// 	return Object.keys(dictionary).filter((word) => word.startsWith(lastWord));
+	// }
+
+	// $: if ($dictionary) {
+	// 	suggestions = compileSuggestions(input, caretPos, $dictionary);
+	// }
+
+	// let textArea: HTMLTextAreaElement;
+
+	// let caretRect: DOMRect | undefined = undefined;
+	// let caretPos: number = 0;
+
+	// const MODES = ['Hangul -> Linear Hangul', 'Hangul -> DPRK'];
+</script>
+
+<svelte:head>
+	<title>Korean Script Converter</title>
+</svelte:head>
+
+<main>
+	<h1>Korean Script Converter</h1>
+
+	<div class="options">
+		<select disabled>
+			<option>DPRK</option>
+		</select>
+	</div>
+
+	<div class="container">
+		<textarea class="text-box input-box" bind:value={input} bind:this={textArea} />
+		<div class="convert">
+			<MdiTransferDown class="icon" />
+		</div>
+		<div class="text-box output-box">{output}</div>
+	</div>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 1rem;
+	}
+
+	h1 {
+		text-align: center;
+	}
+
+	.options {
+		display: grid;
+		place-content: center;
+	}
+
+	.options select {
+		padding: 0.5em 1em;
+		border-radius: 10px;
+		font-size: 1rem;
+		font-family: inherit;
+	}
+
+	@font-face {
+		font-family: 'Atkinson Hyperlegible';
+		src: url('/fonts/AtkinsonHyperlegible-Regular.ttf') format('truetype');
+		font-weight: normal;
+		font-style: normal;
+	}
+
+	@import url('https://fonts.googleapis.com/css2?family=family=Nanum+Gothic:wght@400;700&display=swap');
+
+	:root {
+		font-family: 'Atkinson Hyperlegible', sans-serif;
+	}
+
+	textarea {
+		resize: none;
+		font-size: inherit;
+		font-family: inherit;
+	}
+
+	.container {
+		width: 820px; /* 800px -> 820px to account for new 10px padding */
+		max-width: 100%;
+		margin: 0 auto;
+		padding: 0 1em; /* adds a horizontal padding for smaller screens */
+		box-sizing: border-box; /* prevents the horizontal padding from causing overflow */
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+	}
+
+	.convert {
+		appearance: none;
+		-webkit-appearance: none;
+		-moz-appearance: none;
+
+		background: white;
+		background: rgba(255, 255, 255, 0.9);
+
+		color: rgba(0, 6, 94, 0.9);
+
+		width: 3rem;
+		height: 3rem;
+		margin: -1.5rem auto;
+		margin-bottom: -1rem;
+		border: none;
+		border-radius: 50%;
+
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: 1.75rem;
+		position: relative;
+		z-index: 3;
+
+		box-shadow: 1px 2px 3px 3px rgba(0, 0%, 0%, 10%);
+		-webkit-box-shadow: 1px 2px 3px 3px rgba(0, 0, 0, 0.1); /* Chrome, Safari, Firefox, IE, Opera, ... */
+		-moz-box-shadow: 1px 2px 3px 3px rgba(0, 0, 0, 0.1); /* earlier versions of Firefox*/
+	}
+
+	.text-box {
+		appearance: none;
+		-webkit-appearance: none;
+		-moz-appearance: none;
+
+		padding: 1.2em 1.7em;
+		border-radius: 10px;
+		border: none;
+		width: 100%;
+		box-sizing: border-box; /* fixes overflow issue on smaller screens; box-sizing does not inherit */
+		min-height: calc((100vh - 25rem) / 2);
+
+		backdrop-filter: blur(10px);
+		-webkit-backdrop-filter: blur(10px); /* Safari */
+
+		box-shadow: 2px 2px 1px 5px rgba(0, 0%, 0%, 1%);
+		-webkit-box-shadow: 2px 2px 5px 5px rgba(0, 0, 0, 0.03); /* Chrome, Safari, Firefox, IE, Opera, ... */
+		-moz-box-shadow: 2px 2px 5px 5px rgba(0, 0, 0, 0.03); /* earlier versions of Firefox*/
+	}
+
+	.input-box {
+		background: white;
+		background: rgba(255, 255, 255, 0.8);
+		color: black;
+		color: rgb(0, 6, 94); /* For browsers that don't support RGB percentages */
+		color: rgb(0% 2.46% 36.8%);
+	}
+
+	.output-box {
+		/* makes the output box look different from the input box to avoid user confusion */
+		background: black;
+		background: rgba(
+			0,
+			6,
+			65,
+			0.6
+		); /* basically the text color for .input-box, but with some transparency, and with 
+					            the blue component toned down a bit */
+		color: #eee;
+		white-space: break-spaces;
+		word-break: break-all;
+	}
+</style>
